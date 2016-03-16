865b8175a9605dc17e27243035d0cf07cf953d21
N1nj4R4bb1D <ninja.rabbid@gmail.com>
2016-03-16 13:13:12 +0200

    Small rewrite & cleanup

--- TimeFlow/TimeFlow.cs
+++ TimeFlow/TimeFlow.cs
@@ -11,7 +11,6 @@ namespace TimeFlow
         public static ModConfig TimeFlowConfig { get; private set; }
         public double timeCounter = 0;
         public double lastGameTimeInterval = 0;
-        public string lastLocationName = "";
         public int TenMinuteTickInterval = 7;
 
         [Subscribe]
@@ -19,20 +18,22 @@ namespace TimeFlow
         {
             TimeFlowConfig = new ModConfig();
             TimeFlowConfig = (ModConfig)Config.InitializeConfig(PathOnDisk + "\\Config.json", TimeFlowConfig);
-            Console.WriteLine("The config file for TimeFlow has been loaded."+
+            /*
+            Console.WriteLine("The config file for TimeFlow has been loaded.\n"+
                 "\n\tTickIntervalOutside: {0}"+
                 "\n\tTickIntervalInside: {1}"+
                 "\n\tTickIntervalInMines: {2}"+
                 "\n\tFreezeTimeInMines: {3}",
-                TimeFlowConfig.TickIntervalOutside, TimeFlowConfig.TickIntervalInside, TimeFlowConfig.TickIntervalInMines, TimeFlowConfig.FreezeTimeInMines);
-            Console.WriteLine("TimeFlow Initialization Completed");
-            TenMinuteTickInterval = TimeFlowConfig.TickIntervalInside;
+                TimeFlowConfig.TickIntervalOutdoors, TimeFlowConfig.TickIntervalIndoors, TimeFlowConfig.TickIntervalInMines, TimeFlowConfig.FreezeTimeInMines);
+            Console.WriteLine("\nTimeFlow Initialization Completed");
+            */
+            TenMinuteTickInterval = TimeFlowConfig.TickIntervalIndoors;
         }
 
         [Subscribe]
         public void Pre10MinuteClockUpdateCallback(Pre10MinuteClockUpdateEvent @event)
         {
-            Console.WriteLine("TimeFlow : 10MinuteClockUpdate : " + DateTime.Now.ToString("HH:mm:ss.ffff"));
+            //Console.WriteLine("TimeFlow : 10MinuteClockUpdate : " + DateTime.Now.ToString("HH:mm:ss.ffff"));
             var location = @event.Root.CurrentLocation;
             if (location != null && !location.IsOutdoors && ((location.Name.Equals("UndergroundMine") || location.Name.Equals("FarmCave")) && TimeFlowConfig.FreezeTimeInMines))
             {
@@ -56,33 +57,28 @@ namespace TimeFlow
                     {
                         case "UndergroundMine":
                         case "FarmCave":
-                            timeCounter = Math.Abs(TimeFlowConfig.TickIntervalInMines * fraction);
-                            proportion = Math.Abs(7 * timeCounter / TimeFlowConfig.TickIntervalInMines);
+                            timeCounter = (!TenMinuteTickInterval.Equals(TimeFlowConfig.TickIntervalInMines)) ? Math.Abs(TimeFlowConfig.TickIntervalInMines * fraction) : timeCounter;
+                            TenMinuteTickInterval = TimeFlowConfig.TickIntervalInMines;
                             break;
                         default:
-                            timeCounter = Math.Abs(TimeFlowConfig.TickIntervalInside * fraction);
-                            proportion = Math.Abs(7 * timeCounter / TimeFlowConfig.TickIntervalInside);
+                            timeCounter = (!TenMinuteTickInterval.Equals(TimeFlowConfig.TickIntervalIndoors)) ? Math.Abs(TimeFlowConfig.TickIntervalIndoors * fraction) : timeCounter;
+                            TenMinuteTickInterval = TimeFlowConfig.TickIntervalIndoors;
                             break;
                     }
                 else
                 {
-                    timeCounter = Math.Abs(TimeFlowConfig.TickIntervalOutside * fraction);
-                    proportion = Math.Abs(7 * timeCounter / TimeFlowConfig.TickIntervalOutside);
+                    timeCounter = (!TenMinuteTickInterval.Equals(TimeFlowConfig.TickIntervalOutdoors)) ? Math.Abs(TimeFlowConfig.TickIntervalOutdoors * fraction) : timeCounter;
+                    TenMinuteTickInterval = TimeFlowConfig.TickIntervalOutdoors;
                 }
+
+                proportion = Math.Abs(7 * timeCounter / TenMinuteTickInterval);
                 @event.Root.GameTimeInterval = Convert.ToInt32(proportion);
                 lastGameTimeInterval = @event.Root.GameTimeInterval;
-                if (proportion % 10 == 0)
-                    Console.WriteLine("TimeFlow : " + @event.Root.CurrentLocation.Name+
-                        " : " + Convert.ToInt32(proportion).ToString() + "/7000"+
-                        " : " + DateTime.Now.ToString("HH:mm:ss.ffff"));
                 /*
-                if (LocationChangeCheck(@event.Root.CurrentLocation.Name, @event.Root.CurrentLocation.IsOutdoors))
-                {
-                    timeCounter = Math.Abs(TenMinuteTickInterval * fraction);
-                    Console.WriteLine("TimeFlow : LocationChange : " + @event.Root.CurrentLocation.Name+
-                        " : TickInterval = " + TenMinuteTickInterval.ToString()+
-                        " : " + DateTime.Now.ToString("HH:mm:ss.ffff"));
-                }
+                if (lastGameTimeInterval % 100 == 0)
+                    Console.WriteLine("TimeFlow : " + @event.Root.CurrentLocation.Name + " : " + DateTime.Now.ToString("HH:mm:ss.ffff") +
+                        "\n\ttimeCounter : " + Convert.ToInt32(timeCounter).ToString() + "/" + (TenMinuteTickInterval * 1000).ToString() +
+                        "\n\tproportion : " + Convert.ToInt32(proportion).ToString() + "/7000");
                 */
             }
         }
@@ -90,16 +86,16 @@ namespace TimeFlow
 
     public class ModConfig : Config
     {
-        public int TickIntervalOutside { get; set; }
-        public int TickIntervalInside { get; set; }
+        public int TickIntervalOutdoors { get; set; }
+        public int TickIntervalIndoors { get; set; }
         public int TickIntervalInMines { get; set; }
         public bool FreezeTimeInMines { get; set; }
 
         public override Config GenerateBaseConfig(Config baseConfig)
         {
-            TickIntervalOutside = 21;
-            TickIntervalInside = 28;
-            TickIntervalInMines = 28;
+            TickIntervalOutdoors = 21;
+            TickIntervalIndoors = 28;
+            TickIntervalInMines = 35;
             FreezeTimeInMines = false;
 
             return this;
